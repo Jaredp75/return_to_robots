6f2a855d3fcc7d9c20ea2490e0941bb55b65fb46
Jaredp75 <jaredp75@hotmail.com>
2017-08-28 15:52:51 -0400

    Another commit

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const path = require('path');
 const mustacheExpress = require('mustache-express');
 const mustache = require('mustache');
 const mongodb = require('mongodb');
-const data = require('./data.js');
+const data = require('./robot_data.json');
 //var bodyParser = require('body-parser');
 
+
+
+const MongoClient = mongodb.MongoClient;
+const mongoURL = 'mongodb://localhost:27017/newdb';
+
 const app = express();
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
@@ -14,13 +20,50 @@ app.set('view engine', 'mustache');
 app.use(express.static( __dirname + '/public'));
 
 
-//Listening on root
-app.get('/users', function (req, res) {
-      res.render('users', data)
+//
+app.get('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({}).toArray(function (err, docs) {
+      res.render('index', {robots: docs})
+      })
+    })
 });
 
 //app.get('/', function(req, res){
 //  res.sendFile(path.join(__dirname + '/index.mustache'));
+
+app.get('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": null}).toArray(function (err, docs) {
+      res.render("", {robots: docs})
+    })
+  })
+});
+
+
+
+app.use('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/', function (req, res) {
+  res.render('index', {robots: data.robots});
+});
+
+app.get('/:username', function (req, res) {
+  var user = data.robots.find(function(x) {
+    return x.username === req.params.username
+  });
+  res.render('robots', {user: user});
+});
+
 
 
 
