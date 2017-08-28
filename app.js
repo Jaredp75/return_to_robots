601ca5b40f27682a798a369e42da405e4248ca1a
Jaredp75 <jaredp75@hotmail.com>
2017-08-28 13:45:04 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const mustache = require('mustache');
+const mongodb = require('mongodb');
+const data = require('./data.js');
+//var bodyParser = require('body-parser');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(express.static( __dirname + '/public'));
+
+
+//Listening on root
+app.get('/users', function (req, res) {
+      res.render('users', data)
+});
+
+//app.get('/', function(req, res){
+//  res.sendFile(path.join(__dirname + '/index.mustache'));
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
